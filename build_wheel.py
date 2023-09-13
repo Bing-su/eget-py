5dd8378857334796345a2cfea5b5eb87b6786c4b
Bingsu <ks2515@naver.com>
2023-09-13 21:44:27 +0900

    fix: manylinux

--- build_wheel.py
+++ build_wheel.py
@@ -23,12 +23,12 @@ def main():
         ("windows", "amd64", "win_amd64"),
         ("darwin", "amd64", "macosx_10_7_x86_64"),
         ("darwin", "arm64", "macosx_11_0_arm64"),
-        ("linux", "amd64", "manylinux_2_17_x86_64"),
+        ("linux", "amd64", "manylinux2014_x86_64"),
         ("linux", "amd64", "musllinux_1_1_x86_64"),
-        ("linux", "arm64", "manylinux_2_17_aarch64"),
+        ("linux", "arm64", "manylinux2014_aarch64"),
         ("linux", "arm64", "musllinux_1_1_aarch64"),
-        ("linux", "s390x", "manylinux_2_17_s390x"),
-        ("linux", "ppc64le", "manylinux_2_17_ppc64le"),
+        ("linux", "s390x", "manylinux2014_s390x"),
+        ("linux", "ppc64le", "manylinux2014_ppc64le"),
     ]
 
     for os_, arch, platform in matrix:
